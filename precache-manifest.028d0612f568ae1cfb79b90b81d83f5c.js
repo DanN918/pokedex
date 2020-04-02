d97007105504c756f5fe8b7738c4658fa63ba0c7
DanN918 <51687144+DanN918@users.noreply.github.com>
2020-04-02 00:08:13 -0400

    Updates

---
+++ precache-manifest.028d0612f568ae1cfb79b90b81d83f5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626234bbb5889a28619cabf23fec34ab",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "4b34703eb1b13be21049",
+    "url": "/pokedex/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9dfbc0f9a640d4b5b9ff",
+    "url": "/pokedex/static/css/main.d15bdb3f.chunk.css"
+  },
+  {
+    "revision": "4b34703eb1b13be21049",
+    "url": "/pokedex/static/js/2.458663d1.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/pokedex/static/js/2.458663d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfbc0f9a640d4b5b9ff",
+    "url": "/pokedex/static/js/main.84f94aba.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  },
+  {
+    "revision": "04d8761ee530521599e01968d6fb7363",
+    "url": "/pokedex/static/media/pokeLogo.04d8761e.jpg"
+  }
+]);
